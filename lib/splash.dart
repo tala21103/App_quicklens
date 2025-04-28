de3bf2654e4735dd6f0108f958d323fb959fe530
Deema127 <dee71223@gmail.com>
2025-04-28 22:49:47 +0300

    Add files via upload

---
+++ lib/splash.dart
@@ -0,0 +1,29 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_corse/screens/item_grid_screen.dart';
+
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+    children: [
+      // Positioned.fill(child: Image.asset(
+      //   "assets/images/splashscreen.png",
+      //   fit: BoxFit.cover,),),
+      const Positioned.fill(child: Scaffold()
+      ,),
+        AnimatedSplashScreen(
+          splash: Image.asset("assets/images/quickgif.gif"),
+          nextScreen: const ItemGridScreen(),
+          duration: 3000,
+          splashTransition: SplashTransition.fadeTransition,
+          backgroundColor: Colors.transparent,),
+    ],
+    );
+  }
+
+}
+
