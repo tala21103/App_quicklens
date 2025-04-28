df8e98df8bbf63456095a9b4307c998a3e1cba97
Deema127 <dee71223@gmail.com>
2025-04-28 23:05:55 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:quicklens/app_quicklens.dart';
 import 'package:quicklens/firebase_options.dart';
+import 'package:quicklens/splash.dart';
 
 
 void main() async {
@@ -9,6 +10,14 @@ void main() async {
    
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform);
-  runApp(AppQuicklens());
+  runApp(const MyApp());
 }
-
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context){
+    return const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: SplashScreen(),
+ );
+  }}
